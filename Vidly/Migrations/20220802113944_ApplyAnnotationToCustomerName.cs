2dba7b741e5ec3ef15a3582bb2d2669b69f24a63
Isaac Frank <codingwithisaac@gmail.com>
2022-08-02 13:13:06 +0100

    add constraints

---
+++ Vidly/Migrations/20220802113944_ApplyAnnotationToCustomerName.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Migrations
+{
+    public partial class ApplyAnnotationToCustomerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; } 
         public bool IsSubscribedToNewsletter { get; set; } 
         public MembershipType MembershipType { get; set; } 
